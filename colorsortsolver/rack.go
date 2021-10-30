daf1059e0e7584c86e4159b4b843b73567d44e0f
tfofurn <scott.ventura@gmail.com>
2021-10-30 17:21:02 -0500

    Multithreading and significantly reduced logging

--- colorsortsolver/rack.go
+++ colorsortsolver/rack.go
@@ -16,7 +16,6 @@ func RackFromCSV(input string) Rack {
 
 	csvReader := csv.NewReader(strings.NewReader(input))
 	tubeNames, _ := csvReader.Read()
-	fmt.Println(tubeNames)
 	tubes := make([]Tube, len(tubeNames))
 	for iTube, name := range tubeNames {
 		tubes[iTube] = NewTube(name)
@@ -30,7 +29,6 @@ func RackFromCSV(input string) Rack {
 		for iColor, colorName := range colors {
 			tube := tubes[iColor]
 			color := ColorFromString(colorName)
-			// fmt.Printf("BottomFill %v into %v\n", color, tube.Describe())
 			if color != Empty && color != UnknownColor {
 				tube.BottomFill(color)
 			}
@@ -38,23 +36,16 @@ func RackFromCSV(input string) Rack {
 		}
 	}
 
-	for _, tube := range tubes {
-		fmt.Println(tube)
-	}
-
 	return Rack{make([]string, 0), tubes}
 }
 
 func (r *Rack) Move(sourceIndex, destinationIndex int) Rack {
 	color, amount := r.tubes[sourceIndex].TopColor()
-	moveDescription := fmt.Sprintf("%10vx%v:%s->%s", StringFromColor(color), amount, r.tubes[sourceIndex].name, r.tubes[destinationIndex].name)
+	moveDescription := fmt.Sprintf("%12v × %v: %s -> %s", StringFromColor(color), amount, r.tubes[sourceIndex].name, r.tubes[destinationIndex].name)
 
 	steps := make([]string, len(r.steps), len(r.steps)+1)
 	copy(steps, r.steps)
 	steps = append(steps, moveDescription)
-	if len(steps) < 6 {
-		fmt.Println(steps)
-	}
 	tubes := make([]Tube, 0)
 	for _, tube := range r.tubes {
 		tubes = append(tubes, tube.Copy())
@@ -69,7 +60,7 @@ func (r *Rack) Move(sourceIndex, destinationIndex int) Rack {
 	return Rack{steps: steps, tubes: tubes}
 }
 
-func (r *Rack) AttemptSolution() bool {
+func (r *Rack) AttemptSolution(solutionChannel chan []string) bool {
 	solved := false
 	for srcIndex, srcTube := range r.tubes {
 		for destIndex, destTube := range r.tubes {
@@ -78,9 +69,13 @@ func (r *Rack) AttemptSolution() bool {
 			}
 			if destTube.CanReceiveFrom(srcTube) {
 				postMoveRack := r.Move(srcIndex, destIndex)
-				solved = solved || postMoveRack.CheckSolved()
+				solved = solved || postMoveRack.CheckSolved(solutionChannel)
 				if !solved {
-					postMoveRack.AttemptSolution()
+					if len(r.steps) == 0 {
+						go postMoveRack.AttemptSolution(solutionChannel)
+					} else {
+						postMoveRack.AttemptSolution(solutionChannel)
+					}
 				}
 			}
 			if solved {
@@ -91,12 +86,13 @@ func (r *Rack) AttemptSolution() bool {
 			continue
 		}
 	}
+
 	return solved
 }
 
-func (r *Rack) CheckSolved() bool {
+func (r *Rack) CheckSolved(solutionChannel chan []string) bool {
 	capped, empty, mixed := 0, 0, 0
-	for index, _ := range r.tubes {
+	for index := range r.tubes {
 		tube := r.tubes[index]
 		switch {
 		case tube.IsCapped():
@@ -108,10 +104,7 @@ func (r *Rack) CheckSolved() bool {
 		}
 	}
 	if mixed == 0 {
-		fmt.Printf("Solved! c %d e %d m %d: \n", capped, empty, mixed)
-		for _, step := range r.steps {
-			fmt.Printf("  %s\n", step)
-		}
+		solutionChannel <- r.steps
 		return true
 	}
 	return false
--- main.go
+++ main.go
@@ -4,15 +4,45 @@ import (
 	"colorsortsolver/colorsortsolver"
 	"fmt"
 	"io/ioutil"
+	"math"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
+func solutionListener(solutionsChannel chan []string) {
+	count := 0
+	shortestSolution := math.MaxInt
+
+	printer := message.NewPrinter(language.English)
+
+	for solution := range solutionsChannel {
+		count++
+
+		if len(solution) < shortestSolution {
+			printer.Printf("Solution %d, %d steps\n", count, len(solution))
+			for index, step := range solution {
+				fmt.Printf("%4d: %s\n", index+1, step)
+			}
+			shortestSolution = len(solution)
+		}
+
+		if count%1000000 == 0 {
+			printer.Printf("Solution %d, %d steps\n", count, len(solution))
+		}
+	}
+}
+
 func main() {
-	fileContents, err := ioutil.ReadFile("c:/Users/Scott/Downloads/SortPuz #255 - Sheet1.csv")
+	fileContents, err := ioutil.ReadFile("sample/inciting-incident.csv")
 	if err != nil {
 		fmt.Print(err)
 	}
 	fileContentsString := string(fileContents)
-	// fmt.Print(fileContentsString)
 	baseRack := colorsortsolver.RackFromCSV(fileContentsString)
-	baseRack.AttemptSolution()
+
+	solutionsChannel := make(chan []string, 100)
+
+	baseRack.AttemptSolution(solutionsChannel)
+	solutionListener(solutionsChannel)
 }
