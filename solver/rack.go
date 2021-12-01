a6d65aca61ca67a6f751988bf00ba7a9d5425d0f
tfofurn <scott.ventura@gmail.com>
2021-11-30 21:32:08 -0600

    Check Solved returns on first not-solved tube.

--- solver/rack.go
+++ solver/rack.go
@@ -51,7 +51,6 @@ func RackFromCSV(colorMap *ColorMap, input string) Rack {
 func (r *Rack) Move(sourceIndex, destinationIndex int) Rack {
 	color, amount := r.tubes[sourceIndex].TopColor()
 
-	
 	tubes := make([]Tube, len(r.tubes))
 	copy(tubes, r.tubes)
 	sourceTube := tubes[sourceIndex].Copy()
@@ -71,7 +70,8 @@ func (r *Rack) Move(sourceIndex, destinationIndex int) Rack {
 }
 
 func (r *Rack) AttemptSolution(channels Channels) bool {
-	if len(r.steps) == 1 {
+	const workerStartLen = 1
+	if len(r.steps) == workerStartLen {
 		channels.WorkerCount <- 1
 	}
 	solved := false
@@ -87,13 +87,13 @@ func (r *Rack) AttemptSolution(channels Channels) bool {
 			if destTube.CanReceiveFrom(srcTube) {
 				postMoveRack := r.Move(srcIndex, destIndex)
 				if postMoveRack.tubes[srcIndex].IsEmpty() && postMoveRack.tubes[destIndex].IsCapped() {
-					solved = solved || postMoveRack.CheckSolved(channels)
+					solved = postMoveRack.CheckSolved(channels)
 				}
 				if !solved {
-					if len(r.steps) == 0 {
+					if len(postMoveRack.steps) == workerStartLen {
 						go postMoveRack.AttemptSolution(channels)
 					} else {
-						solved = solved || postMoveRack.AttemptSolution(channels)
+						solved = postMoveRack.AttemptSolution(channels)
 					}
 				}
 			}
@@ -106,30 +106,21 @@ func (r *Rack) AttemptSolution(channels Channels) bool {
 		}
 	}
 
-	if len(r.steps) == 1 {
+	if len(r.steps) == workerStartLen {
 		channels.WorkerCount <- -1
 	}
 	return solved
 }
 
 func (r *Rack) CheckSolved(channels Channels) bool {
-	capped, empty, mixed := 0, 0, 0
 	for index := range r.tubes {
 		tube := r.tubes[index]
-		switch {
-		case tube.IsCapped():
-			capped += 1
-		case tube.IsEmpty():
-			empty += 1
-		default:
-			mixed += 1
+		if !(tube.IsEmpty() || tube.IsCapped()) {
+			return false
 		}
 	}
-	if mixed == 0 {
-		channels.Solutions <- r.steps
-		return true
-	}
-	return false
+	channels.Solutions <- r.steps
+	return true
 }
 
 func (r *Rack) TubeCount() int {
